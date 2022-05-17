c8c84093fb8b61c11be9dff53077f938f380bf9e
Yousef Kotp <yousefkotp@outlook.com>
2022-05-17 19:35:17 +0200

    fixed few bugs

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
         APIBackend api = APIBackend.createTopologyAPI();
 
         api.readJSON("topology.json");    //The api stores the array of topologies within it
-        ArrayList<Topology> topologies = api.queryTopologies();
-        ArrayList<Component> components = api.queryDevices("top1");
-        components = api.queryDevicesWithNetlistNode("t1","res1");
-         api.deleteTopology("top1");
-         api.writeJSON("result.json");
+        //ArrayList<Topology> topologies = api.queryTopologies();
+        //ArrayList<Component> components = api.queryDevices("top1");
+        //components = api.queryDevicesWithNetlistNode("t1","res1");
+         //api.deleteTopology("top1");
+         //api.writeJSON("result.json");
 
     }
 }
