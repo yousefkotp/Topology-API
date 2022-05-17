8f7e7a1190e6bd3d041bebcf7834b1f8a65309ec
Yousef Kotp <yousefkotp@outlook.com>
2022-05-18 00:15:06 +0200

    Testing statements added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         TopologyAPI api = TopologyAPI.createTopologyAPI();
         Topology t1 = api.readJSON("topology.json");
+
+                            //Testing statements
+        //ArrayList<Topology> topologies = api.queryTopologies();
+        //ArrayList<Component> components = api.queryDevices("top1");
+        //component = api.queryDevicesWithNetlistNode("top1","res1");
+        //api.deleteTopology("top1");
+
         api.writeJSON("top1");
         Topology t2 = api.readJSON("top1.json");
-        // Comparing the two objects, note that we have already overridden the "equals" method
-        System.out.println(t1);
-        System.out.println(t2);
+        // Comparing the two objects using overridden equals method
         System.out.println(t1.equals(t2));
     }
 }
